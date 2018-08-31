3cfe717289bb5db87e2b8670bf3201ac98aaac48
M Abdul Aziz <muhammadabdulaziz@gmail.com>
2018-08-31 15:23:17 +0700

    first commit

--- App.js
+++ App.js
@@ -8,27 +8,20 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image} from 'react-native';
-
-/*const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});*/
+import Judul from './Components/Judul';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Judul/>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.welcome}>Nama  : Muhammad Abdul Aziz</Text>
         <Text style={styles.welcome}>Kelas : XI RPL 1</Text>
         <Text style={styles.welcome}>No  : 23</Text>
-        <Image source={require('./fotoku.jpg')}/>
+        <Image style = {{width : 300, height : 400}}
+        source={require('./fotoku.jpg')}/>
       </View>
     );
   }
@@ -46,5 +39,4 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  
 });
